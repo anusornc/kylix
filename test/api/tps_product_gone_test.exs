4c54287bbdaa563bad13dbbf618f6cfb52140651
Anusorn Chaikaew <anusorn.c@crru.ac.th>
2026-09-12 15:20:19 +0700

    feat: TPS modules and dashboard benchmarks are gone (#140)
    Delete the TPS run module and visualizer. Strip Performance Benchmarks,
    Tx/sec, Cache Performance, and /run-benchmark from the dashboard. Submit
    Transaction and the query box stay.

---
+++ test/api/tps_product_gone_test.exs
@@ -0,0 +1,23 @@
+defmodule Kylix.API.TpsProductGoneTest do
+  use ExUnit.Case, async: true
+
+  test "TPS run module is not loaded" do
+    refute Code.ensure_loaded?(Kylix.Benchmark.TransactionSpeed)
+  end
+
+  test "TPS visualizer is not loaded" do
+    refute Code.ensure_loaded?(Kylix.Benchmark.ResultVisualizer)
+  end
+
+  test "dashboard is not a TPS page" do
+    html = Kylix.API.Dashboard.render()
+
+    refute html =~ "Performance Benchmarks"
+    refute html =~ "Tx/sec"
+    refute html =~ "Cache Performance"
+    refute html =~ "/run-benchmark"
+
+    assert html =~ "Submit Transaction"
+    assert html =~ "Run Query"
+  end
+end
